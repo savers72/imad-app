6619ad436542ad7f0cc6592236413258a83ebbdd
savers72 <31322127+savers72@users.noreply.github.com>
2018-02-27 01:07:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 var element = document.getElementById('main.text');
 element.innerHTML = 'SUDHA SINGH RAJPUT';
 
+//move the image
+var img = document.getElementById('asd');
+img.onclick = function(){
+    img.style.marginLeft ='200px';
+    
+}
