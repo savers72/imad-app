b910528e0af7ef07bf34e0eb684fe6bb2fcebdd3
savers72 <31322127+savers72@users.noreply.github.com>
2018-02-26 23:17:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -30,6 +30,9 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/nain.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 
 
 app.get('/ui/madi.png', function (req, res) {
